61f8e9ce0c1f954bbbcb1765e7b3ccc7143c459c
n-i-x <440158+n-i-x@users.noreply.github.com>
2025-03-26 19:42:08 +0000

    add Sound3D check

--- .github/workflows/scripts/check-table-ini.py
+++ .github/workflows/scripts/check-table-ini.py
@@ -3,23 +3,38 @@ import sys
 
 
 def check_backbuffer_scale(config):
-    if not config.has_option('Standalone', 'BackBufferScale'):
+    if not config.has_option("Standalone", "BackBufferScale"):
         return
 
     backbuffer_scale = config.get("Standalone", "BackBufferScale")
 
-    allowed_values = ["1.0", "0.833333", "0.666666", "0.5", "0.416666", "0.355729", "0.333333"]
+    allowed_values = [
+        "1.0",
+        "0.833333",
+        "0.666666",
+        "0.5",
+        "0.416666",
+        "0.355729",
+        "0.333333",
+    ]
     if backbuffer_scale not in allowed_values:
         print(f"Invalid BackBufferScale value: {backbuffer_scale}")
         print(f"Allowed values: {', '.join(allowed_values)}")
         sys.exit(1)
 
 
+def check_sound3d(config):
+    if config.has_option("Player", "Sound3D"):
+        print(f"Sound3D should not be defined in table.ini")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     table_ini = sys.argv[1]
-    
+
     config = configparser.ConfigParser()
     config.read(table_ini)
 
     # Run checks
     check_backbuffer_scale(config)
+    check_sound3d(config)
