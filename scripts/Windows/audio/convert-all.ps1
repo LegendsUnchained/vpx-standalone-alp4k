1ea0663d39e9574f2ea4d62bc491f820dc084454
evilwraith <evilwraith@gmail.com>
2026-09-10 19:22:19 -0400

    POV Cleanup + Folder cleanup
    123
    24
    JPTAF
    AORAB
    Al's Garage Band
    Aladdin's Castle
    Ali
    Alice in Wonderland
    Amazon Hunt
    Andromeda
    Apache
    Asterix
    Asteroid Annie
    JP AFM
    JP AFM LE
    Austin Powers
    JP Avengers
    Avengers Pro
    
    Delete vpx-aladdincastle(duplicate)
    Add Diner FullDMD Backglass
    
    Delete Windows audio scripts

--- scripts/Windows/audio/convert-all.ps1
+++
@@ -1,2 +0,0 @@
-# Calls converrto-mono.ps1 to batch convert all tables in its folder - Boris
-Get-ChildItem -Path "Path\To\Tables\Folder" -File -Filter "*.vpx" | ForEach-Object { .\converrto-mono.ps1 -table $_.Name }
